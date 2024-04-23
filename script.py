cc8a17dba9a65b3e23c9833b947e3db2b0ac79b4
nodem5 <167844569+nodem5@users.noreply.github.com>
2024-04-23 19:55:32 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import socket
+
+app = FastAPI()
+
+# Endpoint "health" zwracający status 200
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+# Endpoint "get-hostname" zwracający nazwę hosta
+@app.get("/get-hostname")
+async def get_hostname():
+    hostname = socket.gethostname()
+    return {"hostname": hostname}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
