d3f64d6ebd296e768d81209df86f9418f97c5a7b
GregoryO <dummy_email@gmail.com>
2024-04-24 11:45:49 +0200

    fixes

--- script.py
+++ script.py
@@ -3,10 +3,12 @@ import socket
 
 app = FastAPI()
 
+
 # Endpoint "health" zwracający status 200
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
 
 # Endpoint "get-hostname" zwracający nazwę hosta
 @app.get("/get-hostname")
@@ -14,6 +16,8 @@ async def get_hostname():
     hostname = socket.gethostname()
     return {"hostname": hostname}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8080)
